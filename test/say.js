f91178c5b2ae6c6de2127ee77e94f0d398dd6e83
Titi <alextitieni@yahoo.com>
2021-09-08 11:10:51 +0300

    covered numbers between 0 - 19

--- test/say.js
+++ test/say.js
@@ -3,7 +3,12 @@ const {expect, assert} = require("chai");
 const {say} = require("../src/say");
 
 describe("Say problem tests", function(){
-    it("should say 'zero'", function(){
-        expect(say(0)).to.equal("zero");
-;    })
+    describe("Numbers between 0 - 19", function(){
+        it("should say 'zero'", function(){
+            expect(say(0)).to.equal("zero");
+    ;    })
+        it("should say 'one'", function(){
+            expect(say(1)).to.equal("one");
+        })
+    });
 });
---
+++ test/test.js
@@ -0,0 +1,3 @@
+dictio = {1: "unu", 3: "trei"};
+
+console.log(2 in dictio);
